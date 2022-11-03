16474a056e223ca76c9a71c85398ae06a42b4d1d
Michael Guedko <mjgeko@gmail.com>
2022-11-03 16:54:11 -0400

    Add automatic ship placement

--- scripts/ship.js
+++ scripts/ship.js
@@ -5,25 +5,48 @@ class Ship {
         this.hits = 0;
         this.sunk = false;
         this.head = options.head || [null, null];
-        this.rotation = options.rot || 'down';
+        this.rotation = options.rot || 1;
         this.coord = null;
-        this.createCoords = function () {
-            //TODO: Fix coords getting generated out of bounds
+        this.automaticShipPosition = function () {
+            // Checks for the first valid rotation of the ship
+            //  down:1 left:2 up:3 right:4
             const coordArr = [];
             let hx = this.head[0];
             let hy = this.head[1];
             for (let index = 0; index < this.length; index++) {
-                if (this.rotation == 'up') {
-                    coordArr.push([hx, hy--]);
-                }
-                if (this.rotation == 'down') {
-                    coordArr.push([hx, hy++]);
+                if (hx > 10 || hy > 10 || hx < 0 || hy < 0) {
+                    index = 0;
+                    hx = this.head[0];
+                    hy = this.head[1];
+                    if (this.rotation == 4) {
+                        console.log("No placeable position!");
+                    } else {
+                        this.rotation++;
+                    }
+
+                } else {
+                    //Up
+                    if (this.rotation == 3) {
+                        coordArr.push([hx, hy--]);
+                    }
+                    //Down
+                    if (this.rotation == 1) {
+                        coordArr.push([hx, hy++]);
+                    }
+                    //Left
+                    if (this.rotation == 2) {
+                        coordArr.push([hx--, hy]);
+                    }
+                    //Right
+                    if (this.rotation == 4) {
+                        coordArr.push([hx++, hy]);
+                    }
                 }
             }
             this.coord = coordArr;
             return (coordArr);
         }
-        this.createCoords();
+        this.automaticShipPosition();
     }
 
     hit(int) {
