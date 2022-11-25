0eb92c48d7a4fc0bc1e1fd8bc09d336ecfb3b330
Michael Guedko <mjgeko@gmail.com>
2022-11-25 12:39:46 -0500

    Refactor game function into class object

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
-import {Ship} from "./ship.js"
-import {Player} from "./player.js"
-import {Gameboard} from "./gameboard.js"
+import {
+    Ship
+} from "./ship.js"
+import {
+    Player
+} from "./player.js"
+import {
+    Gameboard
+} from "./gameboard.js"
 
 
 /*
@@ -8,13 +14,16 @@ import {Gameboard} from "./gameboard.js"
     - Implement displaying of hits and misses in DOM [2/2] 
         ✅ hits/misses in enemy board 
         ✅ hits/misses in player board 
+    ✅ Refactor game function into Class
     - Implement game loop:
-        ❌ taking turns
+        ❌ start game modal
+        ✅ taking turns
         - win/lose modal
     - Create console div for displaying if the move hit/miss/sunk
     - Move DOMinterface into separate module
     - Implement placement system for ships
     - Add pictures/textures to ships
+    - Implement smart AI to improve attacks
 
     BUGS:
     ✅ Fix ships generating off grid
@@ -28,9 +37,9 @@ class DOMinterface {
         this.shotArea = document.querySelector('.shot-container');
         this.shipArea = document.querySelector(".ship-container");
         this.debugFlag = false;
-    }   
+    }
     //Refactor into one renderShips() method
-    renderPlayerShips(shipArr){
+    renderPlayerShips(shipArr) {
         console.log('Player Ships');
         console.log(shipArr);
         for (let index = 0; index < shipArr.length; index++) {
@@ -48,7 +57,7 @@ class DOMinterface {
             }
         }
     }
-    renderEnemyShips(shipArr){
+    renderEnemyShips(shipArr) {
         console.log('Computer Ships');
         console.log(shipArr);
         for (let index = 0; index < shipArr.length; index++) {
@@ -66,12 +75,12 @@ class DOMinterface {
             }
         }
     }
-    createShipSquares(){
+    createShipSquares() {
         for (let irow = 0; irow < 10; irow++) {
             for (let icolumn = 0; icolumn < 10; icolumn++) {
                 const square = document.createElement('div');
-                square.style.gridColumn = icolumn+1;
-                square.style.gridRow = irow+1;
+                square.style.gridColumn = icolumn + 1;
+                square.style.gridRow = irow + 1;
                 square.id = `P${icolumn}-${irow}`;
                 square.classList.add('item');
                 //square.addEventListener("click",this.shotHandler);
@@ -79,13 +88,13 @@ class DOMinterface {
             }
         }
     }
-    
-    createShotSquares(){
+
+    createShotSquares() {
         for (let irow = 0; irow < 10; irow++) {
             for (let icolumn = 0; icolumn < 10; icolumn++) {
                 const square = document.createElement('div');
-                square.style.gridColumn = icolumn+1;
-                square.style.gridRow = irow+1;
+                square.style.gridColumn = icolumn + 1;
+                square.style.gridRow = irow + 1;
                 square.id = `${icolumn}-${irow}`;
                 square.classList.add('item');
                 square.addEventListener("click", (event) => {
@@ -101,42 +110,39 @@ class DOMinterface {
             }
         }
     }
-    mouseHoverHandler(event){
+    mouseHoverHandler(event) {
         const element = event.target;
         const arr = [...element.id];
         const x = parseInt(arr[0]) + 1;
         const y = parseInt(arr[2]) + 1;
         const result = compBoard.checkValidMove(x, y);
-        if(result[0]){
+        if (result[0]) {
             //Valid move
             element.classList.add('valid');
-        }
-        else if(!result[0] && result[1] == 2){
+        } else if (!result[0] && result[1] == 2) {
             //invalid move (hit)
             element.classList.add('invalid');
-        }
-        else if(!result[0] && result[1] == 1){
+        } else if (!result[0] && result[1] == 1) {
             //invalid move (miss)
             element.classList.add('invalidmiss');
         }
     }
-    mouseLeaveHandler(event){
+    mouseLeaveHandler(event) {
         const element = event.target;
         element.classList.remove('valid');
         element.classList.remove('invalid');
         element.classList.remove('invalidmiss');
     }
-    shotHandler(event){
+    shotHandler(event) {
         const coord = [...event.target.id];
         const x = parseInt(coord[0]) + 1;
         const y = parseInt(coord[2]) + 1;
         console.log(x, y);
         const result = compBoard.checkValidMove(x, y);
-        if(result[0]){
-            console.log('Player: '+ pl.play(x,y));
+        if (result[0]) {
+            console.log('Player: ' + pl.play(x, y));
             console.log('Computer: ' + cp.play(...cp.aiPlay()));
-        }
-        else{
+        } else {
             console.log('Duplicate move');
         }
         //Temp for displaying hits/misses
@@ -144,57 +150,54 @@ class DOMinterface {
         this.renderShots(playerBoard);
         this.mouseHoverHandler(event);
     }
-    renderShots(playerObj){
+    renderShots(playerObj) {
         const hits = playerObj.hits;
         const misses = playerObj.misses;
         for (let index = 0; index < misses.length; index++) {
             let coord = misses[index];
-            if(playerObj == playerBoard){
+            if (playerObj == playerBoard) {
                 let element = document.getElementById(`P${coord[0]-1}-${coord[1]-1}`);
                 element.classList.add('miss');
                 element.textContent = '';
-            }
-            else{
+            } else {
                 let element = document.getElementById(`${coord[0]-1}-${coord[1]-1}`);
                 element.classList.add('miss');
                 element.textContent = '';
-            }            
+            }
         }
         for (let index = 0; index < hits.length; index++) {
             let coord = hits[index];
-            if(playerObj == playerBoard){
+            if (playerObj == playerBoard) {
                 let element = document.getElementById(`P${coord[0]-1}-${coord[1]-1}`);
                 element.classList.remove('.ship');
                 element.classList.add('hit');
                 element.textContent = '💥'
-            }
-            else{
+            } else {
                 let element = document.getElementById(`${coord[0]-1}-${coord[1]-1}`);
                 element.classList.add('hit');
                 element.textContent = '💥'
             }
-            
+
         }
     }
-    removeChildren(parentNode){
-        while(parentNode.firstChild){
+    removeChildren(parentNode) {
+        while (parentNode.firstChild) {
             parentNode.removeChild(parentNode.lastChild);
         }
     }
-    debugHandler(shipArr){
+    debugHandler(shipArr) {
         this.debugFlag = !this.debugFlag;
-        if(this.debugFlag == true){
+        if (this.debugFlag == true) {
             console.log('Show');
             this.renderEnemyShips(shipArr);
             this.renderPlayerShips(playerBoard.getShips());
-        }
-        else{
+        } else {
             this.update();
             console.log('Hide');
             return;
         }
     }
-    update(){
+    update() {
         console.log('update');
         //Rendering enemy waters
         this.removeChildren(this.shotArea);
@@ -207,17 +210,19 @@ class DOMinterface {
     }
 }
 // I Dont like these here
-    const compBoard = new Gameboard();
-    const playerBoard = new Gameboard();
-     //Computer player
-     const cp = new Player(playerBoard, compBoard, true);
-     //Player
-     const pl = new Player(playerBoard, compBoard, false);
+const compBoard = new Gameboard();
+const playerBoard = new Gameboard();
+//Computer player
+const cp = new Player(playerBoard, compBoard, true);
+//Player
+const pl = new Player(playerBoard, compBoard, false);
 //Main Game Loop
-    function game(){
-    //TODO: Turn this into a module & add turn, gameEnd method
-    //Initializing objects (Do not put into contructor)
-    const UI = new DOMinterface();
+class Game {
+    constructor(params){
+
+    }
+    init(){
+        const UI = new DOMinterface();
     UI.createShotSquares();
     UI.createShipSquares();
     //Place ships TEMP
@@ -231,39 +236,65 @@ class DOMinterface {
     });
     //Debug button
     const db = document.querySelector('#cheat');
-    db.addEventListener('click', function (e){
+    db.addEventListener('click', function (e) {
         UI.debugHandler(compBoard.getShips());
     });
-    //Check ship data
-    // console.log('Checking computer ships...');
-    // compBoard.checkShipData();
-    // console.log('Checking player ships...');
-    // playerBoard.checkShipData();
+    
 
     UI.renderPlayerShips(playerBoard.getShips());
 
     //Player turn start
+    }
+    turn(){
+
+    }
+    gameEnd(){
+
+    }
 }
 
-function createShips(){
-    //TODO: Fix bug where battleship generates with a length of 3
-    //TODO: Fix ships still intersecting sometimes
+function createShips() {
     let shiparr = [];
-    const carrier = {name: "Carrier", length: 5, head: genCoords() };
-    const battleship = {name: "Battleship", length: 4, head: genCoords()};
-    const destroyer = {name: "Destroyer", length: 3, head: genCoords()};
-    const submarine = {name: "Submarine", length: 3, head: genCoords()};
-    const patrolboat = {name: "Patrol Boat", length: 2, head: genCoords()};
-    shiparr.push(carrier,battleship,destroyer,submarine,patrolboat);
-    return(shiparr);
+    const carrier = {
+        name: "Carrier",
+        length: 5,
+        head: genCoords()
+    };
+    const battleship = {
+        name: "Battleship",
+        length: 4,
+        head: genCoords()
+    };
+    const destroyer = {
+        name: "Destroyer",
+        length: 3,
+        head: genCoords()
+    };
+    const submarine = {
+        name: "Submarine",
+        length: 3,
+        head: genCoords()
+    };
+    const patrolboat = {
+        name: "Patrol Boat",
+        length: 2,
+        head: genCoords()
+    };
+    shiparr.push(carrier, battleship, destroyer, submarine, patrolboat);
+    return (shiparr);
 }
-function genCoords(){
+
+function genCoords() {
     const x = randomIntFromInterval(1, 10);
     const y = randomIntFromInterval(1, 10);
-    return [x,y]
+    return [x, y]
 }
+
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-game();
-export {genCoords};
+let g = new Game();
+g.init();
+export {
+    genCoords
+};
